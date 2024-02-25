2a2122cd61743de36e6685afbf701249eea2b073
15476 <1547656148@qq.com>
2024-02-25 20:57:07 +0800

    2222

--- CoreWebDemo/obj/Debug/net7.0/CoreWebDemo.AssemblyInfo.cs
+++ CoreWebDemo/obj/Debug/net7.0/CoreWebDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoreWebDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ca51063187d60793a770653f9968858c5b3b69c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoreWebDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoreWebDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
